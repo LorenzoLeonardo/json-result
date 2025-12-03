87343d846db3168b8aeaee0bacf5eb546e835c22
Lorenzo Leonardo <enzotechcomputersolutions@gmail.com>
2025-12-03 22:23:02 +0800

    Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
 use serde::de::{DeserializeOwned, Error};
 
+/// A generic enum representing a JSON result that can either be a success (`Ok`) with a value of type `T`
+/// or an error (`Err`) with a value of type `E`.
+///
+/// This enum is designed to be serialized and deserialized using Serde's untagged enum representation,
+/// allowing it to seamlessly handle JSON values that could match either type.
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 #[serde(untagged)]
 pub enum JsonResult<T, E> {
+    /// Variant representing a successful result containing a value of type `T`.
     Ok(T),
+    /// Variant representing an error result containing a value of type `E`.
     Err(E),
 }
 
@@ -12,6 +19,19 @@ where
     T: serde::Serialize,
     E: serde::Serialize,
 {
+    /// Converts a `JsonResult<T, E>` into a `serde_json::Value`.
+    ///
+    /// Serializes the contained value in either the `Ok` or `Err` variant into a JSON value.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use serde_json::json;
+    /// # use your_crate::JsonResult;
+    /// let res: JsonResult<i32, String> = JsonResult::Ok(42);
+    /// let json_value: serde_json::Value = res.into();
+    /// assert_eq!(json_value, json!(42));
+    /// ```
     fn from(res: JsonResult<T, E>) -> Self {
         match res {
             JsonResult::Ok(v) => serde_json::json!(v),
@@ -27,6 +47,27 @@ where
 {
     type Error = serde_json::Error;
 
+    /// Attempts to convert a `serde_json::Value` into a `JsonResult<T, E>` by
+    /// trying to deserialize it first into `T` (success variant), then into `E` (error variant).
+    ///
+    /// If deserialization into both types fails, returns a combined error message detailing both failures.
+    ///
+    /// # Errors
+    ///
+    /// Returns a `serde_json::Error` if the input JSON value cannot be parsed as either `T` or `E`.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # use serde_json::json;
+    /// # use your_crate::JsonResult;
+    /// let json_val = json!(42);
+    /// let res: JsonResult<i32, String> = json_val.try_into().unwrap();
+    /// match res {
+    ///     JsonResult::Ok(val) => assert_eq!(val, 42),
+    ///     JsonResult::Err(_) => panic!("Expected Ok variant"),
+    /// }
+    /// ```
     fn try_from(value: serde_json::Value) -> Result<Self, Self::Error> {
         let t_res = serde_json::from_value::<T>(value.clone());
         let e_res = serde_json::from_value::<E>(value);
